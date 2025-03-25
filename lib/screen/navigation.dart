48aad6248b5066123b16abbcdabe719b945e965f
Azlan-saja <azlansaja19@gmail.com>
2025-03-25 15:47:00 +0700

    p9

--- lib/screen/navigation.dart
+++ lib/screen/navigation.dart
@@ -4,7 +4,9 @@ import 'package:master/screen/navigation/navigation_satu.dart';
 import 'package:master/screen/navigation/navigation_tiga.dart';
 
 class Navigation extends StatelessWidget {
-  const Navigation({super.key});
+  Navigation({super.key});
+
+  final terimaData = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -12,48 +14,73 @@ class Navigation extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Layar Navigation'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const NavigationSatu(
-                      namaAnda: 'Azlansaja',
+      body: Padding(
+        padding: const EdgeInsets.all(14),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const NavigationSatu(
+                        namaAnda: 'Azlansaja',
+                      ),
+                    ),
+                  );
+                },
+                child: const Text("1. Kirim Data"),
+              ),
+              const Divider(),
+              TextFormField(
+                controller: terimaData,
+                textAlign: TextAlign.center,
+                readOnly: true,
+                decoration: const InputDecoration(
+                  labelText: 'Terima Data?',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(50),
                     ),
                   ),
-                );
-              },
-              child: const Text("1. Kirim Data"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const NavigationDua(),
-                  ),
-                );
-              },
-              child: const Text("2. Terima Data"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const NavigationTiga(),
-                  ),
-                );
-              },
-              child: const Text(
-                "3. Kirim Data dan Terima Balasan/Data",
+                ),
               ),
-            ),
-          ],
+              ElevatedButton(
+                onPressed: () async {
+                  final hasil = await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const NavigationDua(),
+                    ),
+                  );
+
+                  if (hasil != null) {
+                    // Ada data
+                    terimaData.text = hasil;
+                  } else {
+                    // Tidak ada data
+                    terimaData.clear();
+                  }
+                },
+                child: const Text("2. Terima Data"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const NavigationTiga(),
+                    ),
+                  );
+                },
+                child: const Text(
+                  "3. Kirim Data dan Terima Balasan/Data",
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
